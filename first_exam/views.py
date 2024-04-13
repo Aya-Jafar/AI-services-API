cc60b0bffcf6d985787169c2a00f337756b19acb
Aya-jafar <aya.jafar002@gmail.com>
2024-04-13 17:34:21 +0300

    College Branch

---
+++ first_exam/views.py
@@ -0,0 +1,10 @@
+# Create your views here.
+from django.http import HttpResponse
+
+
+# Create your views here.
+def MyExamView(request):
+    return HttpResponse('Welcome....create a new app with name (fitst exam) that show this sentence!!')
+
+
+
