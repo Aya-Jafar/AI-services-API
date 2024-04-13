51dd534ae9b8ceb4623e7e5302570aaa7e97e22a
Aya-jafar <aya.jafar002@gmail.com>
2024-04-13 23:27:11 +0300

    Rename main app

--- AI_services/apps.py
+++ AI_services/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class MyAppConfig(AppConfig):
+class AIServicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_app'
+    name = 'AI_services'
 
