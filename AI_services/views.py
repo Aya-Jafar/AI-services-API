dd1dc37aadd3cf226e4a96f3604efe30b2aca7de
Aya-jafar <aya.jafar002@gmail.com>
2024-04-20 00:39:08 +0300

    Add summary model endpoint & preprocess response

--- AI_services/views.py
+++ AI_services/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM
 import json
 import torch
 
@@ -51,27 +51,33 @@ def predict_next_word(request):
 """
     Summarizarion model
 """
+# mohammedRiad/flanT5_summary_withPEFT
 
-# summary_tokenizer = AutoTokenizer.from_pretrained("mohammedRiad/flanT5_summary_withPEFT")
-# summary_model = AutoModelForCausalLM.from_pretrained("mohammedRiad/flanT5_summary_withPEFT")
+summary_tokenizer = AutoTokenizer.from_pretrained("mohammedRiad/_____")
+summary_model = AutoModelForSeq2SeqLM.from_pretrained("mohammedRiad/_____")
 
-# def summarize(request):
-#     if request.method == 'POST':
-#         data = json.loads(request.body)
-#         prompt = data.get('prompt', '')
-#         # Tokenize the prompt
-#         input_ids = next_word_tokenizer.encode(prompt, return_tensors="pt")
-#         attention_mask = torch.ones(input_ids.shape, dtype=torch.long)
 
-#         max_length = len(input_ids[0]) + 10 
-#         # Generate text
-#         output = next_word_model.generate(input_ids,pad_token_id=next_word_tokenizer.pad_token_id,max_length=max_length, attention_mask=attention_mask, num_return_sequences=1, temperature=0.5,do_sample=True)
-#         # Decode the generated output
-#         generated_text = next_word_tokenizer.decode(output[0], skip_special_tokens=True)
-#         print(generated_text)
-#         return JsonResponse({'generated_text': generated_text})
-#     else:
-#         return JsonResponse({'error': 'POST method required'})
+@csrf_exempt
+def summarize(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        prompt = data.get('long_text', '')
+        # Tokenize the long text
+        input_ids = summary_tokenizer.encode(prompt, return_tensors="pt", max_length=1024, truncation=True)
+        attention_mask = torch.ones(input_ids.shape, dtype=torch.long)
+
+        max_length = len(input_ids[0]) // 2
+    
+        # Generate text
+        output = summary_model.generate(input_ids,pad_token_id=summary_tokenizer.pad_token_id,max_length=max_length, attention_mask=attention_mask, num_return_sequences=1, temperature=0.5,do_sample=True)
+
+        # Decode the generated output
+        summarized_text = summary_tokenizer.decode(output[0], skip_special_tokens=True)
+        # print(summarized_text)
+
+        return JsonResponse({'summarized_text': summarized_text})
+    else:
+        return JsonResponse({'error': 'POST method required'})
 
 
 
@@ -111,9 +117,13 @@ def send_question(request):
         output = next_word_model.generate(input_ids,pad_token_id=next_word_tokenizer.pad_token_id,max_length=max_length, attention_mask=attention_mask, num_return_sequences=1, temperature=0.5)
         # Decode the generated output
         generated_text = next_word_tokenizer.decode(output[0], skip_special_tokens=True)
-        return JsonResponse({'generated_text': generated_text})
+        # Remove duplucated question tokens
+        end_index = generated_text.index(prompt) + len(prompt)
+        remaining_text = ''.join(generated_text[end_index:]).strip()
+        remaining_text = remaining_text.replace('"', '')
+        remaining_text = remaining_text.replace("?", "")
+        return JsonResponse({'generated_text': remaining_text})
     else:
         return JsonResponse({'error': 'POST method required'})
     
-
 
